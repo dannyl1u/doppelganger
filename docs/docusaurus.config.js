308be87a9b5617331d99a2ec706ff4c54495cc5f
dannyl1u <dannyjialiliu@gmail.com>
2024-10-28 13:29:21 -0700

    title+tagline

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Doppelgänger',
+  tagline: 'Automatic duplicate issue and PR detection for GitHub repositories',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
